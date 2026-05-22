8ac8d250123983bce98d8b26a5cad7ec9fa94e77
Rogério Castro <air@MacBook-Air-de-Air.local>
2026-05-21 23:31:53 -0300

    refactor(pastoral): Disponibilidade e Histórico como submenus de Escala

--- core/init.js
+++ core/init.js
@@ -10,7 +10,7 @@
 
 (function() {
   var xhr = new XMLHttpRequest();
-  xhr.open("GET", "views/sidebar.html?v=6.30.75", false);
+  xhr.open("GET", "views/sidebar.html?v=6.30.76", false);
   xhr.send(null);
   if (xhr.status >= 200 && xhr.status < 300) {
     var login = document.getElementById("login-screen");
